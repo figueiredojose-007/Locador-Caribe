02782706e5b5e6fe0a1dea58451569c72c2dcd91
figueiredojose-007 <jose464646ff@gmail.com>
2025-02-19 11:48:31 -0300

    Update login.py

--- login.py
+++ login.py
@@ -24,31 +24,34 @@ def validar_login():
 #criando janela:
 janela = Tk()
 janela.title('Locadora Caribe')
-janela.geometry('300x300')
+janela.geometry('740x460')
 janela.iconbitmap('Imagens/logoProjetoIco.ico')
+janela.configure(background='#1b1b33')
+janela.attributes('-toolwindow', False)
+janela.resizable(False, False)
 
-
-
+display = Frame(janela, borderwidth=1, highlightbackground="white", highlightthickness=2, background="#3c3d61")
+display.place(width=720,height=440, x=10, y=10)
 
 
 
 
 #colocando elementos da janela:
-label = Label(janela, text='Faça seu login')
+label = Label(display, text='Bem-vindo!', font=('Arial', 40, 'bold'), background='#3c3d61', foreground='white')
 label.pack(pady=5)
 
-campo_usuario = Label(janela, text='Usuário:')
+campo_usuario = Label(display, text='Usuário:', font=('Arial', 20, 'bold'), background='#3c3d61', foreground='white')
 campo_usuario.pack(pady=5)
 
-entradausuario = Entry(janela)
+entradausuario = Entry(display)
 entradausuario.pack(pady=5)
 
-campo_senha = Label(janela, text='Senha:')
+campo_senha = Label(display, text='Senha:', font=('Arial', 20, 'bold'), background='#3c3d61', foreground='white')
 campo_senha.pack(pady=5)
-entradasenha = Entry(janela)
+entradasenha = Entry(display, show='*')
 entradasenha.pack(pady=5)
 
-botao = Button(janela, text='Entrar', command=validar_login)
+botao = Button(display, text='Entrar', command=validar_login)
 botao.pack(pady=5)
 
 
