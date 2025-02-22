7661335a658317bfb3e27513c3bba67bd9fa1569
figueiredojose-007 <jose464646ff@gmail.com>
2025-02-22 13:48:18 -0300

    alteraçao gozo

--- Arquivos/Classes/classItem.py
+++ Arquivos/Classes/classItem.py
@@ -15,7 +15,7 @@ class Item:
     def getId(self):
         return self._id
     
-    def infoItem(self, display):
+    def infoItem(self, display, intemSelecionado):
         global itemSelecionado
         itemSelecionado = self
         item = self
@@ -35,3 +35,24 @@ class Item:
         capaLabel = Label(display, image=capa)
         capaLabel.image = capa  
         capaLabel.pack(side="left", padx=10,anchor="n")  
+
+        # Exibir info dos itens: nome, tipo, ano, estoque e disponibilidade
+        nome = Label(display, text=item.titulo, font=("Arial", 16, "bold"), 
+                        background="#3c3d61", foreground="white", wraplength=230, justify="left")
+        nome.pack(pady=10, anchor="w")
+        
+        tipo = Label(display, text=f"Tipo: {item.tipo}", font=("Arial", 12),
+                        background="#3c3d61", foreground="white", justify="left")
+        tipo.pack(pady=5, anchor="w")
+        ano = Label(display, text=f"Ano: {item.ano}", font=("Arial", 12),
+                        background="#3c3d61", foreground="white", justify="left")
+        ano.pack(pady=5, anchor="w")
+        
+        estoque = Label(display, text=f"Estoque: {item.estoque}", font=("Arial", 12),
+                        background="#3c3d61", foreground="white", justify="left")
+        estoque.pack(pady=5, anchor="w")
+        
+        disponibilidade = Label(display, text=f"Disponível: {item.disponib}", font=("Arial", 12),
+                        background="#3c3d61", foreground="white", justify="left")
+        disponibilidade.pack(pady=5, anchor="w")
+        
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Locador Caribe
+# Locadora Caribe
  Projeto final realizado no último bimestre do ano letivo 2024, segundo ano do curso técnico em informática.
