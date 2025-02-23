04d1f323eff688a6f558c9050287a74331a64637
figueiredojose-007 <jose464646ff@gmail.com>
2025-02-23 14:53:54 -0300

    cuzeta

---
+++ Arquivos/Classes/classFilme.py
@@ -0,0 +1,7 @@
+from classItem import item
+class Filme(item):
+    tipo = "Filme" 
+    def __init__(self, titulo, ano, tipo, disponib=True, idNum=0, descricao="Descrição não definida", duracao=0, genero="Gênero não definido", versao3d=False, diretor="Diretor não definido"):
+        super().__init__(titulo, ano, tipo, disponib, idNum, descricao)
+        self.versao3d = versao3d
+        self.diretor = diretor
